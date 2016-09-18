9d228c542be9a4c4daeed017b2baa1bc9937ac2f
Tomas Gonzalez <tomasg88@gmail.com>
2016-09-17 21:47:20 -0300

    Form centrado y login inline

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -59,6 +59,10 @@ module.exports = {
                 }); 
             }
         });     
+    },
+    
+    find: function(req, res){
+        
     }
 };
 
--- assets/js/public/signup/SignupController.js
+++ assets/js/public/signup/SignupController.js
@@ -41,7 +41,7 @@
 				 
 			 }).catch(function onError(){
 				 
-			 };
+			 });
 		};
 
 	}
