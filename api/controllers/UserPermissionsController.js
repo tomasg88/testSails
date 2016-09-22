049ee2f7846b88ef57f4ed2f5831338d6458450d
Tomas Gonzalez <tomasg88@gmail.com>
2016-09-22 00:04:24 -0300

    Loguea user y agrega relacion user-permiso

---
+++ api/controllers/UserPermissionsController.js
@@ -0,0 +1,11 @@
+/**
+ * PermissionsController
+ *
+ * @description :: Server-side logic for managing permissions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
